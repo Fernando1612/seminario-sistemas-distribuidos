0e528e7b48fc0f8a9778bb30ca80201df58bfd92
Fernando Maceda <fernando.patricio.maceda@gmail.com>
2022-09-15 20:08:09 -0500

    Update test.py

--- consistent_hash/test.py
+++ consistent_hash/test.py
@@ -6,81 +6,101 @@
 #
 # Distributed under terms of the MIT license.
 
-from CHash import *
-from ModHash import *
-from Store import *
+
 """
-Creates a Store that is managed using a consistent hash.
-
-We store a bunch of words in the datastore, then remove one node from the store
-and then add it back.
+This class represents a Resource (or a record) that is stored in one of the
+nodes of the datastore.
 """
+class Resource:
+    def __init__(self, name):
+        self.name = name
 
 
-def read_words(fname):
-  """
-  Just read a bunch of words from a file.
-  """
-  result = []
-  with open(fname, 'r') as f:
-    for word in f:
-      word = word.split('/')[0]
-      word = word.strip()
-      result.append(word)
-
-  return result
+"""
+This class represents a Node, the place were resources (or records) are stored
+in the datastore.
+"""
+class Node:
+    def __init__(self, name):
+        self.name = name
+        self.resources = []
 
 
-def run(words):
-  """
-  We create an object representing the hash scheme that we are willing to use.
-  The hash object is then passed to the Store constructor. When adding elements
-  to the store, the selected hash scheme is used to determine where to place
-  the records.
-  """
-  my_hash = CHash()
-  my_store = Store(my_hash)
-  
-  
-  """
-  Add three nodes to the Store
-  """
-  my_store.add_node("Node 1")
-  my_store.add_node("Node 2")
-  my_store.add_node("Node 3")
-  
-  my_store.dump()
-  
+"""
+The class Store represents a distributed datastore. Each datastore stores a
+number of resources. Resources are assigned to nodes based on a hash schema.
+"""
+class Store:
+    def __init__(self, hash_generator):
+        self.nodes = {}
+        self.hash_generator = hash_generator
 
-  """
-  Save all words in the Store
-  """
-  for word in words:
-      my_store.add_resource(word)
-  
-  my_store.dump()
+    def dump(self):
+        """
+        Prints the contents of each of the nodes that make up the distributed
+        datastore.
+        """
+        print("===== STORE =====")
+        print("Using scheme: {0}".format(self.hash_generator.get_name()))
+        self.hash_generator.dump()
+        for node in self.nodes.keys():
+            print('[{0} ({1} items)]'.format(self.nodes[node].name, len(self.nodes[node].resources)))
+            for resource in self.nodes[node].resources:
+                print('    - {0}'.format(resource))
 
+    def add_node(self, new_node):
+        """
+        Creates a new node in the datastore. Once the node is created, a number
+        of resources have to be migrated to conform to the hash schema.
+        Important: Notice that this method is designed to work with a
+        consistent hash. You may need to adjust it to make it work with modular
+        hash. Hash_generator has a member "scheme_name" that you can use.
+        """
+        prev_node = self.hash_generator.hash(new_node)
+        migraciones = 0
 
-  """
-  Remove one node from the Store. Stored objects need to be migrated to the
-  remaining nodes.
-  """
-  my_store.remove_node("Node 1")
-  my_store.dump()
+        rc = self.hash_generator.add_node(new_node)
+        if rc == 0:
+            self.nodes[new_node] = Node(new_node)
 
+            """
+            If there is a node in the counter clockwise direction, then the
+            resources stored in that node need to be rebalanced (removed from a
+            node and added to another one).
+            """
+            if prev_node is not None:
+                resources = self.nodes[prev_node].resources.copy()
 
-  """
-  Add the node back to the Store. Objects need to be migrated to conform to the
-  Hash scheme.
-  """
-  my_store.add_node("Node 1")
-  my_store.dump()
+                for element in resources:
+                    target_node = self.hash_generator.hash(element)
 
+                    if target_node is not None and target_node != prev_node:
+                        migraciones += 1
+                        self.nodes[prev_node].resources.remove(element)
+                        self.nodes[target_node].resources.append(element)
+            print("Numero de migraciones ", migraciones)
 
+    def remove_node(self, node):
+        """
+        Removes a node from the datastore. When a node is removed, a number
+        of resources (that were stored in that node) have to be migrated to
+        conform to the hash schema.
+        Important: Notice that this method is designed to work with a
+        consistent hash. You may need to adjust it to make it work with modular
+        hash. Hash_generator has a member "scheme_name" that you can use.
+        """
+        rc = self.hash_generator.remove_node(node)
+        if rc == 0:
+            for element in self.nodes[node].resources:
+                self.add_resource(element)
+            del self.nodes[node]
 
-if __name__ == '__main__':
+    def add_resource(self, res):
+        """
+        Add a new resource (record) to the distributed datastore. The record is
+        added to a node that is selected using the hash strategy.
+        """
+        target_node = self.hash_generator.hash(res)
 
-  words = read_words('words_alpha.txt')
-  words = words[:100]
-
-  run(words)
+        if target_node is not None:
+            self.nodes[target_node].resources.append(res)
